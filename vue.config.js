2a626bc283ee7b0493917c9a7abd1083c9f0996b
luolei <1074366993@qq.com>
2020-10-28 00:51:20 +0800

    add login and logout

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,5 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ],
-  devServer: {
-    host: 'localhost',
-    port: 8080,
-    proxy: {
-      '/api': {
-        target: "therainisme.com:8088",
-        changeOrigin: true,
-        pathRewrite: {
-          '^/api': '/api'
-        }
-      }
-    }
-  },
+  ]
 }
